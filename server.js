ee988b67fbc78518cce3f24a8a5558786c8f81e4
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-06-22 22:08:56 -0400

    to do list v2

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
+let items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log('Server Started on port 3000!!!');
 });
 
 app.get('/', (req, res) => {
-
   let today = new Date();
 
   let options = {
@@ -21,6 +23,15 @@ app.get('/', (req, res) => {
   let day = today.toLocaleDateString('en-US', options);
 
   res.render('list', {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItem: items
   });
+});
+
+app.post('/', (req, res) => {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect('/');
 });
