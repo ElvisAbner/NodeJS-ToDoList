44c232ebe9a683d4cea2ca615a37425bd40dcb6b
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-08-24 15:37:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.{MONGO_URI});
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
