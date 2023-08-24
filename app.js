570de17c258b52a4c2a4de3244879d4613712405
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-08-24 20:08:47 +0000

    final commit

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const mongoose = require ("mongoose")
 const _ = require('lodash')
 require('dotenv').config()
 
+
 const app = express();
 const PORT = process.env.PORT || 3000
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://admin-elvis:100billionx@cluster0.zocew6p.mongodb.net/?retryWrites=true&w=majority');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
 }
-
-app.all('*', (req,res) => {
-  res.json({"every thing":"is awesome"})
-})
 
 connectDB().then(() => {
   app.listen(PORT, () => {
