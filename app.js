0d255b8f306a879f6403e836bf4bab5e1eb5ace5
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-08-24 19:12:53 +0000

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require ("mongoose")
 const _ = require('lodash')
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 3000
